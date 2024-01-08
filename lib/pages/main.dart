02acd923089b0a87449f1907b774d04c317d35b6
Meyyaou <104941102+Meyyaou@users.noreply.github.com>
2024-01-08 19:51:02 +0100

    Update main.dart
    last main

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,31 +1,53 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:projetiam/firebase_options.dart';
-//import 'package:file_picker/file_picker.dart';
-//import 'package:google_sign_in/google_sign_in.dart';
-//import 'package:firebase_storage/firebase_storage.dart';
-//import 'package:cloud_firestore/cloud_firestore.dart';
-//import 'package:firebase_auth/firebase_auth.dart';
-import 'login.dart';
+import 'package:hawas_biya_algeria_guide/models/user.dart';
+import 'package:hawas_biya_algeria_guide/onBoardingPages/splashscreen_wrapper.dart';
+import 'package:hawas_biya_algeria_guide/services/authentication.dart';
+import 'package:provider/provider.dart';
+import 'services/firebase_options.dart';
 
-void main() async {
+
+void main() async{
+
+  //in case du mal à se connect on doit init
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(MyApp());
+  ); //IMPORTANT DONT DELETE APP CHECK ERROR
+ /* await FirebaseAppCheck.instance.activate(
+    androidProvider: AndroidProvider.playIntegrity,
+  );*/
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]).then((value)=> runApp(MyApp()));
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'HAWAS-BIYA algeriaguide',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
+    return StreamProvider<AppUser?>.value(
+      value: AuthenticationService().user,
+      initialData: null,
+      child: MaterialApp(
+        title: 'Onboarding screens for the app',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: SplashScreenWrapper(),
       ),
-      home: login(),
     );
+    //TODO should i add aroute generator??
   }
 }
+
