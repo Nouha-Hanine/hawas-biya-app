0d2d4fe71dff4ffbd2341720319ee2c120596fbb
Meyyaou <104941102+Meyyaou@users.noreply.github.com>
2024-01-08 20:04:47 +0100

    Update map.dart
    without bottom nav

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -27,63 +27,8 @@ class _MapState extends  State<Map> {
           ],
         ),
       ),
-      bottomNavigationBar: _BottomNavigationBar(context),
     );
   }
 
 
-
-  Widget _BottomNavigationBar(BuildContext context) {
-    return BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.explore,color: Colors.green),
-          label: 'Discover',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.map,color: Colors.green),
-          label: 'Map',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.favorite,color: Colors.green),
-          label: 'Favorites',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person,color: Colors.green),
-          label: 'Profile',
-        ),
-      ],
-      currentIndex: selectedIndex,
-      selectedItemColor: Colors.red ,
-      onTap: onItemTapped,
-    );
-  }
-
-
-// Fonction appelée lorsqu'un élément de la barre de navigation est cliqué
-  void onItemTapped(int index) {
-    setState(() {
-      selectedIndex = index;
-
-    });
-
-    // Utilisez Navigator pour naviguer vers la page correspondante
-    switch (index) {
-      case 1:
-      // Vous êtes déjà sur la page Discover, donc pas besoin de navigation ici
-        break;
-      case 0:
-        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => DiscoverPage()));
-        break;
-      case 2:
-        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Favorites()));
-        break;
-      case 3:
-        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Profile()));
-        break;
-      default:
-        break;
-    }
-  }
-}
 
