b557a1318b82d699fe9f91176055ac77db11dee5
Meyyaou <104941102+Meyyaou@users.noreply.github.com>
2024-01-08 19:52:23 +0100

    user model for the db

---
+++ lib/models/user.dart
@@ -0,0 +1,12 @@
+class AppUser {
+  final String uid;
+
+  AppUser(this.uid);
+}
+
+class AppUserData {
+  final String uid;
+  final String name;
+
+  AppUserData({required this.uid, required this.name});
+}
